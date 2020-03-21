72d18a2d3eeb37a7fd677a8c8469817586cda1ec
leventerguder <erguder.levent@gmail.com>
2020-03-21 13:15:45 +0300

    maven based project structure

---
+++ chapter04/aop/src/test/java/com/apress/cems/aop/PersonMonitorTestV5.java
@@ -0,0 +1,69 @@
+/*
+Freeware License, some rights reserved
+
+Copyright (c) 2019 Iuliana Cosmina
+
+Permission is hereby granted, free of charge, to anyone obtaining a copy 
+of this software and associated documentation files (the "Software"), 
+to work with the Software within the limits of freeware distribution and fair use. 
+This includes the rights to use, copy, and modify the Software for personal use. 
+Users are also allowed and encouraged to submit corrections and modifications 
+to the Software for the benefit of other users.
+
+It is not allowed to reuse,  modify, or redistribute the Software for 
+commercial use in any way, or for a user's educational materials such as books 
+or blog articles without prior permission from the copyright holder. 
+
+The above copyright notice and this permission notice need to be included 
+in all copies or substantial portions of the software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.apress.cems.aop;
+
+import com.apress.cems.aop.config.AopConfig;
+import com.apress.cems.aop.service.PersonService;
+import com.apress.cems.aop.test.TestDbConfig;
+import com.apress.cems.dao.Person;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Iuliana Cosmina
+ * @since 1.0
+ */
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AopConfig.class, TestDbConfig.class})
+class PersonMonitorTestV5 {
+
+    @Autowired
+    PersonService personService;
+
+    @Test
+    void testBadSave() {
+        var person = new Person();
+        person.setId(3L);
+        person.setUsername("nancy.drew");
+        person.setFirstName("Nanc#");
+        person.setLastName("&rew");
+        person.setPassword("1@#$asta");
+        person.setHiringDate(LocalDateTime.now());
+        assertThrows(IllegalArgumentException.class, () -> personService.save(person));
+    }
+
+
+}
---
+++ chapter07/boot-sec/src/test/java/com/example/bootsec/BootSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
